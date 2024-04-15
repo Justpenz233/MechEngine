5e0e4d3aaf13ea89e53220785bd1a6717547874f
SiqiLi <j2152662@126.com>
2024-04-15 19:11:21 +0800

    Make luisa a submodule, avoid re-download

--- Engine/3rd/luisacompute.cmake
+++
@@ -1,8 +0,0 @@
-include(FetchContent)
-FetchContent_Declare(
-        LuisaCompute
-        GIT_REPOSITORY    https://github.com/LuisaGroup/LuisaCompute.git
-        GIT_TAG           stable
-)
-
-FETCHCONTENT_MAKEAVAILABLE(LuisaCompute)
