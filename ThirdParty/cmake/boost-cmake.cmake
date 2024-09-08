a280b6de11005e4212e3292a6aec92a0a3ede5ad
Siqi Li <siqi_li@mymail.sutd.edu.sg>
2024-09-08 10:51:58 +0800

    Fix cmake warning

--- ThirdParty/cmake/boost-cmake.cmake
+++ ThirdParty/cmake/boost-cmake.cmake
@@ -1,7 +1,10 @@
 include(FetchContent)
+cmake_policy(SET CMP0135 NEW)
+
 set(BOOST_INCLUDE_LIBRARIES system foreach any variant graph property_map multiprecision heap)
 FetchContent_Declare(
         Boost
         URL https://github.com/boostorg/boost/releases/download/boost-1.81.0/boost-1.81.0.tar.xz
+        DOWNLOAD_EXTRACT_TIMESTAMP true
 )
 FetchContent_MakeAvailable(Boost)
