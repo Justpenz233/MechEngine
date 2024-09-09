634b3923eccd15020ff1ccdafa8db5acaa5e295f
WALK-FISH2 <128489949+WALK-FISH2@users.noreply.github.com>
2024-09-09 15:59:19 +0800

    LOG_DEBUG

--- Source/Runtime/Core/Misc/Platform.h
+++ Source/Runtime/Core/Misc/Platform.h
@@ -1,13 +1,45 @@
 //
 // Created by MarvelLi on 2024/3/25.
+// Edited by Rick Zhang on Sep 05, 2024
 //
 
 #pragma once
 
 
-#ifdef ME_BUILD_DEBUG
-#define FORCEINLINE inline 	/* Don't force code to be inline */
+/*#ifdef ME_BUILD_DEBUG*/
+/*#define FORCEINLINE inline*/ 	/* Don't force code to be inline */
+/*#endif*/
+
+
+// Define ME_BUILD_TYPE based on the build type
+#if defined(ME_BUILD_DEBUG)
+	#define ME_BUILD_TYPE "Debug"
+#elif defined(ME_BUILD_RELEASE)
+	#define ME_BUILD_TYPE "Release"
+#elif defined(ME_BUILD_RELWITHDEBINFO)
+	#define ME_BUILD_TYPE "RelWithDebInfo"
+#else
+	#define ME_BUILD_TYPE "Unknown"
 #endif
+
+//Enable LOG_DEBUG if it's a Debug build type
+#if defined(ME_BUILD_DEBUG) || defined(ME_BUILD_RELWITHDEBINFO)
+	#define ENABLE_LOG_DEBUG 1
+#else
+	#define ENABLE_LOG_DEBUG 0
+#endif
+
+
+
+#include <sstream>
+
+
+#if ENABLE_LOG_DEBUG
+	#define LOG_DEBUG(...) MechEngine::Logger::Get().GetDefaultLogger()->debug(__VA_ARGS__)
+#else
+	#define LOG_DEBUG(...) // No operation
+#endif
+
 
 #ifndef FORCEINLINE
 	#if (_MSC_VER >= 1200) //MSVC compiler
