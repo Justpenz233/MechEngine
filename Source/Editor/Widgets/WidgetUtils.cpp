b496b2047e89d1470614328255cad1223df714a6
Marvel Li <siqi_li@mymail.sutd.edu.sg>
2024-12-11 15:49:27 +0800

    Fix UI

--- Source/Editor/Widgets/WidgetUtils.cpp
+++ Source/Editor/Widgets/WidgetUtils.cpp
@@ -61,24 +61,21 @@ void UI::DrawActorPanel(ObjectPtr<class Actor> Actor)
 {
 	std::string HeaderName = UI::GetObjectDisplayName(Actor) + " properties panel";
 
-	if(ImGui::Begin(ICON_FA_INFO "  Actor Panel",nullptr, ImGuiTreeNodeFlags_DefaultOpen))
+	ImGui::Begin(ICON_FA_INFO "  Actor Panel",nullptr, ImGuiTreeNodeFlags_DefaultOpen);
+	ImGui::PushItemWidth(ImGui::GetWindowWidth() * 0.5);
+	if(ImGui::CollapsingHeader((ICON_FA_PEN_TO_SQUARE "  " + UI::GetObjectDisplayName(Actor)).c_str(), ImGuiTreeNodeFlags_DefaultOpen))
 	{
-		ImGui::PushItemWidth(ImGui::GetWindowWidth() * 0.5);
-
-		if(ImGui::CollapsingHeader((ICON_FA_PEN_TO_SQUARE "  " + UI::GetObjectDisplayName(Actor)).c_str(), ImGuiTreeNodeFlags_DefaultOpen))
+		DrawObjectPanel(Actor.get());
+		// Show all component property
+		for (auto Component : Actor->GetAllComponents())
 		{
-			DrawObjectPanel(Actor.get());
-			// Show all component property
-			for (auto Component : Actor->GetAllComponents())
+			if(ImGui::TreeNodeEx((ICON_FA_CUBE "  " + UI::GetObjectDisplayName(Component)).c_str(), ImGuiTreeNodeFlags_DefaultOpen))
 			{
-				if(ImGui::TreeNodeEx((ICON_FA_CUBE "  " + UI::GetObjectDisplayName(Component)).c_str(), ImGuiTreeNodeFlags_DefaultOpen))
-				{
-					DrawObjectPanel(Component.get());
-					ImGui::TreePop();
-				}
+				DrawObjectPanel(Component.get());
+				ImGui::TreePop();
 			}
 		}
-		ImGui::PopItemWidth();
-		ImGui::End();
 	}
+	ImGui::PopItemWidth();
+	ImGui::End();
 }
