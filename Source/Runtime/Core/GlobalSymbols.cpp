119cf1948292548d01a3a210a395690dd1c94e66
Siqi Li <siqi_li@mymail.sutd.edu.sg>
2025-01-05 16:55:24 +0800

    Fix FPS calculation

--- Source/Runtime/Core/GlobalSymbols.cpp
+++ Source/Runtime/Core/GlobalSymbols.cpp
@@ -15,12 +15,17 @@ void CalculateFPSTimings()
 	// Calculate the average frame time via continued averaging.
 	static long long LastTime = 0.0;
 	long long CurrentTime = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch()).count();
+	if (LastTime == 0.) [[unlikely]]
+	{
+		LastTime = CurrentTime;
+		return;
+	}
 	float FrameTimeMS = (float)((CurrentTime - LastTime));
+	LastTime = CurrentTime;
 
 	// A 3/4, 1/4 split gets close to a simple 10 frame moving average
-	GAverageMS = GAverageMS * 0.75f + FrameTimeMS * 0.25f;
+	GAverageMS = GAverageMS == 0.f ? FrameTimeMS : GAverageMS * 0.75f + FrameTimeMS * 0.25f;
 
-	LastTime = CurrentTime;
 
 	// Calculate average framerate.
 	GAverageFPS = 1000.f / GAverageMS;
